9d3329b0377e2e713a53cfb1c882440a40a9b3f0
Kelsey Low <kklowlow@gmail.com>
2019-01-13 04:13:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, name|
     name.each do |key, value|
-      languages[style] = {name: style}
+      languages[name] = {style: style}
   end
   return new_hash
 end
